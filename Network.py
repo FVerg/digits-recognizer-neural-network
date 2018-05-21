aacba84401a4949be29c401b55aaaeb376f8d276
fverg <vergonaf@gmail.com>
2018-05-21 18:32:43 +0200

    Added sigmoid function

--- Network.py
+++ Network.py
@@ -16,3 +16,14 @@ class Network (object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(
             sizes[:-1], sizes[1:])]   # Initial weights, calculated randomly
+        # Usage: weights[0] - Weight matrix between level 0 (Input) and 1 (Hidden)
+        # Usage:  weights[1] - Weight matrix between level 1 (Hidden) and 2 (Output)
+
+    # Function that calculates the sigmoid of the given vector
+    # z = wx + b
+    # sigmoid(z) = 1 / 1 + exp(-z)
+    # Meaning:
+    #  - If z >> 0 -> exp(-z) = 0   -> sigmoid(z) = 1
+    #  - If z << 0 -> exp(-z) = inf -> sigmoid(z) = 0
+    def sigmoid(z):
+        return 1.0/(1.0)+np.exp(-z)
