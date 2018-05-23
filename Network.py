ff1b2f40f4358a4ec3952dbf230c2d9419f8aa71
fverg <vergonaf@gmail.com>
2018-05-23 18:01:17 +0200

    Added some functions

--- Network.py
+++ Network.py
@@ -27,3 +27,21 @@ class Network (object):
     #  - If z << 0 -> exp(-z) = inf -> sigmoid(z) = 0
     def sigmoid(z):
         return 1.0/(1.0)+np.exp(-z)
+
+    # Function that takes the input vector and return the output of the network
+    def feedforward(self, a):
+        # Returns the output of the network if a is an input vector
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+            return a
+
+    """Train the neural network using mini-batch stochastic
+        gradient descent.  The "training_data" is a list of tuples
+        "(x, y)" representing the training inputs and the desired
+        outputs.  The other non-optional parameters are
+        self-explanatory.  If "test_data" is provided then the
+        network will be evaluated against the test data after each
+        epoch, and partial progress printed out.  This is useful for
+        tracking progress, but slows things down substantially."""
+
+    def stochastic_gradient_descent(self, training_data, epochs, mini_batch_size, eta, test_data=None):
