fc51b2218efaefae77d049016d24a2b45bfd5e5f
FVerg <vergonaf@gmail.com>
2018-05-31 18:03:41 +0200

    Added code to create models and to import data into csv files

---
+++ create_model.py
@@ -0,0 +1,57 @@
+# ------------------------------------------------------------------------------
+# This script creates a keras model and saves it in Models folder
+# It has been used for the creation of 10 neural networks, one for each digit
+# in order to recognize whether or not an image (handwritten digit) represent
+# a specific number.
+# ------------------------------------------------------------------------------
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+import numpy as np
+
+np.random.seed(7)
+
+INPUT_NEURONS = 784
+HIDDEN_NEURONS = 10
+OUTPUT_NEURONS = 1
+NUM_EPOCHS = 30
+BATCH_SIZE = 10
+
+# ------------------------------------------------------------------------------
+# Choose the input file, considering the number you want the network to recognize
+# - training_x: ClassLabel = 1 for number x
+# ------------------------------------------------------------------------------
+
+dataset = np.loadtxt(r"mnist_datasets2\training_2.csv", delimiter=',', skiprows=1)
+
+X = dataset[:, 1:785]
+Y = dataset[:, 785]
+
+model = Sequential()
+
+# Layer 1 - Input: 784 Neurons
+model.add(Dense(INPUT_NEURONS, input_dim=INPUT_NEURONS, activation='relu'))
+# Layer 2 - Hidden: 10 Neurons
+model.add(Dense(HIDDEN_NEURONS, activation='sigmoid'))
+# Layer 3 - Output: 1 Neuron (T/F)
+model.add(Dense(OUTPUT_NEURONS, activation='sigmoid'))
+
+# Compile model
+# - binary_crossentropy = Error function
+# - optimizer = Gradient descent algorithm
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Fit the model
+model.fit(X, Y, epochs=NUM_EPOCHS, batch_size=BATCH_SIZE)
+
+# Evaluate the model
+scores = model.evaluate(X, Y)  # Evaluating the model using the same dataset
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
+# Save the model on disk
+# ------------------------------------------------------------------------------
+# Change the output name depending on what number this NN has been trained on.
+# ------------------------------------------------------------------------------
+model.save("Models\model_2.hd5")
--- import_data.py
+++ import_data.py
@@ -29,8 +29,19 @@ for i in range(0, 784):
 
 col_names.append("ClassLabel")
 
-images = tr_d[0].tolist()
-labels = tr_d[1].tolist()
+# -----------------------------------------------------------------------------
+# Change here if you want to create csv related to training or test data:
+# Training data:
+# images = tr_d[0].tolist()
+# images = tr_d[1].tolist()
+
+# Test data:
+# images = te_d[0].tolist()
+# labels = te_d[1].tolist()
+# -----------------------------------------------------------------------------
+
+images = te_d[0].tolist()
+labels = te_d[1].tolist()
 
 for image, label in zip(images, labels):
     image.append(label)
@@ -40,9 +51,8 @@ dataset = pd.DataFrame(images, columns=col_names)
 for i in range(0, 10):
     df = dataset.copy()
     df = format_output(dataset, i)
-    df.to_csv(r"mnist_datasets2\training_"+str(i) + ".csv",
+    # -------------------------------------------------------------------------
+    # Define name for output csv file. Change test and/or training depending
+    # on what you decide to save: training set or test set
+    df.to_csv(r"mnist_datasets2\test_"+str(i) + ".csv",
               sep=',', header=True, columns=col_names, mode='w')
-#dataset = format_output(dataset, 1)
-
-
-# print dataset["ClassLabel"]
